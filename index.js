4c9b9e2ae43c39d052479054236008210c1ad282
Hildemberg Eling <hildembergeling@gmail.com>
2022-09-22 15:39:06 -0300

    formatação

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 //const BrowserWindow = electron.remote.BrowserWindow;
 
 let mainWindow;
-  
+
 // Function to create independent window or main window
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -16,21 +16,20 @@ function createWindow() {
       contextIsolation: false,
     },
     show: false,
-    
   });
-  
+
   // Main window loads index.html file
   mainWindow.loadFile("index.html");
   //tirar menu
   let menu = Menu.buildFromTemplate([]);
   Menu.setApplicationMenu(menu);
-  
+
   // To maximize the window
   mainWindow.maximize();
   mainWindow.show();
 }
-  
-// criar janela filha 
+
+// criar janela filha
 function createcadastrowin() {
   cadastroWindow = new BrowserWindow({
     width: 1000,
@@ -38,45 +37,37 @@ function createcadastrowin() {
     modal: true,
     show: false,
     parent: mainWindow, // adição do parente
-    frame:false,
+    frame: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
       enableRemoteModule: true,
     },
-    
-  
   });
-  
+
   // carregar janela cadastro.html file
   cadastroWindow.loadFile("cadastro.html");
-  
+
   cadastroWindow.once("ready-to-show", () => {
     cadastroWindow.show();
   });
 }
-  
-ipcMain.on("openChildWindow", (event, arg) => {
+
+ipcMain.on("opencadastroWindow", (event, arg) => {
   createcadastrowin();
 });
-
-
-
-
-
-
 
 //on app
 app.whenReady().then(() => {
   createWindow();
-  
+
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
-  
+
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
